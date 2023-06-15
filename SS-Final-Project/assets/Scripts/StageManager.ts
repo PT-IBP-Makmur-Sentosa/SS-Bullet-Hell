fcc9cef25c7c724ecb214e127054ec83f4d2b4ed
stellameku <stellameku@gmail.com>
2023-06-15 15:59:47 +0800

    Revert "add volume etc"
    This reverts commit aeb2d8a099ad424872cf2cffb28be80fd934e172.

--- SS-Final-Project/assets/Scripts/StageManager.ts
+++ SS-Final-Project/assets/Scripts/StageManager.ts
@@ -5,8 +5,8 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-// import "firebase/auth";
-// import "firebase/database";
+import "firebase/auth";
+import "firebase/database";
 
 
 
@@ -42,19 +42,14 @@ export default class StageManager extends cc.Component {
     @property(cc.Prefab)
     bulletPrefab: cc.Prefab = null;
 
-    @property({type:cc.AudioClip})
-    bgm: cc.AudioClip = null;
+    @property(cc.AudioClip)
+    backgroundMusic: cc.AudioClip = null;
+    @property(cc.AudioClip)
+    lobbyMusic: cc.AudioClip = null;
+    @property(cc.AudioClip)
+    gameover_music: cc.AudioClip = null;
 
-    @property(cc.Node)
-    Canvas: cc.Node = null;
-
-    @property(cc.Node)
-    MainCamera: cc.Node = null;
-
-    @property(cc.Node)
-    PauseBoard: cc.Node = null;
-
-    private count: number = 0;
+    private audioSource: cc.AudioSource = null;
 
 
     public bulletPool = new cc.NodePool("Bullet");
@@ -65,16 +60,20 @@ export default class StageManager extends cc.Component {
     }
 
     gameOver() {
+        cc.audioEngine.playEffect(this.gameover_music, false);
         this.gameover.active = true
         this.enemy.active = false
         this.scheduleOnce(function () {
+            cc.audioEngine.playMusic(this.lobbyMusic, true);
             cc.director.loadScene("Lobby")
         } , 2)
         this.saveScoreToFirebase(this.score)
+
     }
 
     onLoad () {
         const initialBulletCount = 100;
+        cc.audioEngine.playMusic(this.backgroundMusic, true)
         for (let i = 0; i < initialBulletCount; i++) {
           const bullet = cc.instantiate(this.bulletPrefab);
           this.bulletPool.put(bullet);
@@ -83,149 +82,10 @@ export default class StageManager extends cc.Component {
             this.loading.active = false;
             this.enemy.active = true;
         }, 2)
-    }
-
-    playBGM(){
-        // ===================== TODO =====================
-        // 1. Play music. The audio clip to play is this.bgm
-        cc.audioEngine.playMusic(this.bgm, true)
-        // ================================================
-
-    }
-
-    resumeBGM(){
-        cc.audioEngine.resumeMusic();
-    }
-
-    Toggle_init(){
-        let checkEventHandler = new cc.Component.EventHandler();
-        checkEventHandler.target = this.node; // 这个 node 节点是你的事件处理代码组件所属的节点
-        checkEventHandler.component = "StageManager";
-        checkEventHandler.handler = "Pause";
-        checkEventHandler.customEventData = "foobar";
-        cc.find("Canvas/level1/UI/Pausebtn").getComponent(cc.Button).clickEvents.push(checkEventHandler);
-        // this.toggle.checkEvents.push(checkEventHandler);
-    }
-
-    resume_init(){
-        let conti = new cc.Component.EventHandler();
-        conti.target = this.node; // 这个 node 节点是你的事件处理代码组件所属的节点
-        conti.component = "StageManager";
-        conti.handler = "Resume";
-        conti.customEventData = "foobar";
-        cc.find("Canvas/pause/Continue").getComponent(cc.Button).clickEvents.push(conti);
-
-        let quit = new cc.Component.EventHandler();
-        quit.target = this.node; // 这个 node 节点是你的事件处理代码组件所属的节点
-        quit.component = "StageManager";
-        quit.handler = "Quit";
-        quit.customEventData = "foobar";
-        cc.find("Canvas/pause/Quit").getComponent(cc.Button).clickEvents.push(quit);
-
-    }
-
-    Resume(event, customEventData){
-        // Destroy pause board and resume the game!
-        this.count = 0;
-        cc.log("Resume");
-        // cc.find("Canvas/pause").destroy();
-        this.enemy.active=true 
-        this.PauseBoard.active=false
-        cc.director.resume();
-        this.resumeBGM();
-    }
-
-    Quit(event, customEventData){
-        // Quit game
-        this.count = 0;
-        cc.find("Canvas/pause").destroy();
-        cc.director.loadScene("setting");
-    }
-
-    Pause(event, customEventData) {
-        // 这里的 toggle 是事件发出的 Toggle 组件
-        // 这里的 customEventData 参数就等于之前设置的 "foobar"
-            // Pause.
-            // Turn off button listener.
-            // this.OFFPause();
-            this.enemy.active=false
-        this.count++;
-        if(this.count==1){
-            // Draw UI Life :
-            // var pause = cc.instantiate(this.PauseBoard);
-            // this.Canvas.addChild(pause);
-            // pause.setPosition(this.MainCamera.x, this.MainCamera.y);
-            this.PauseBoard.active=true
-            // Init resume btns.
-            this.resume_init();
-
-
-            console.log("pause"); 
-            // this.stopBGM();
-            cc.director.pause();
-        }else{
-            cc.log("Cannot press Pause since Now pausing.");
-        }
-}
-
-    IncreaseVolume(event, customEventData) {
-        console.log("increaseVolume_init called");  // Added for debugging
-    
-        let currentVolume = cc.audioEngine.getMusicVolume();
-        console.log(currentVolume)
         
-        cc.audioEngine.setMusicVolume(currentVolume + 0.1);
-
-        if(currentVolume > 1) {
-            cc.audioEngine.setMusicVolume(1);
-
-        }
     }
-
-    increaseVolume_init() {
-        let volumeUp = new cc.Component.EventHandler();
-        volumeUp.target = this.node;
-        volumeUp.component = "StageManager";
-        volumeUp.handler = "IncreaseVolume";
-        
-        volumeUp.customEventData = "foobar";
-        cc.find("Canvas/pause/PlusButtonMsc").getComponent(cc.Button).clickEvents.push(volumeUp);
-    }
-
-    decreaseVolume_init() {
-        let volumeDown =new cc.Component.EventHandler();
-        volumeDown.target = this.node;
-        volumeDown.component = "StageManager";
-        volumeDown.handler = "DecreaseVolume";
-        
-        volumeDown.customEventData = "foobar";
-        cc.find("Canvas/pause/MinusButtonMsc").getComponent(cc.Button).clickEvents.push(volumeDown);
-    }
-
-    DecreaseVolume(event, customEventData) {
-        console.log("decreaseVolume called");  // Added for debugging
-    
-        let currentVolume = cc.audioEngine.getMusicVolume();
-        console.log(currentVolume)
-        
-        cc.audioEngine.setMusicVolume(currentVolume - 0.1);
-
-        if(currentVolume <0) {
-            cc.audioEngine.setMusicVolume(0);
-
-
-        }
-    }
-
-   
-
-   
 
     start () {
-        this.playBGM();
-        this.Toggle_init();
-        this.increaseVolume_init(); 
-        this.decreaseVolume_init();
         this.stageLabel.string = this.stageName;
         this.healthLabel.string = "HP X " + this.player.getComponent("Player").lives.toString()
         this.scoreLabel.string = "Score: " + this.score.toString()
@@ -257,4 +117,3 @@ export default class StageManager extends cc.Component {
     })
 }
 }
-
